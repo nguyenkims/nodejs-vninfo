2573963a737246ac2754f13fe61a94b1ebba3220
Son Nguyen Kim <s.nguyenkim@criteo.com>
2014-07-15 13:25:40 +0200

    countLine synchronous

---
+++ countLine.js
@@ -0,0 +1,20 @@
+// Count lines for each files given in argument
+// Usage
+// node countLine file1 file2 file3 ...
+
+var fs = require('fs');
+var arguments = process.argv.slice(2);
+var now = new Date();
+
+arguments.forEach(function(element, index, array){
+	console.log(element + ":" + countLineSynchro(element));
+});
+
+function countLineSynchro(file){
+	// Benching purpose
+	var milliSecs = new Date().getTime() - now;
+	console.log("read " + file + " at +" + milliSecs + " ms");
+
+	var contents = fs.readFileSync(file, "utf8");
+	return contents.split('\n').length;
+}
