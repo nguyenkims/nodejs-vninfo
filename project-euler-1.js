2dd7b098d5134f0dae97122f6fc5388430a9aebb
Son Nguyen Kim <nguyenkims@hotmail.com>
2014-07-27 17:52:19 +0000

    add doc for project-euler example

--- project-euler-1.js
+++ project-euler-1.js
@@ -1,3 +1,7 @@
+// This example illustrates how we can use JS as a fast scripting language
+// to solve algorithmic problems. The problem here is at https://projecteuler.net/problem=1
+// Just run node project-euler-1 and voilà, you pass the 1st problem of project-euler !
+
 var N = 1000, sum = 0;
 for (var i = 0; i < N ; i ++){
 	if (i % 3 == 0 || i % 5 == 0){
